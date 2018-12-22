45c12f4af0a39ffb8532f5c426bbb66f4fa8f64d
Penumbra <penumbra420@gmail.com>
2018-12-22 17:01:02 -0500

    Updated footer

--- njf/js/main.js
+++ njf/js/main.js
@@ -11,13 +11,14 @@ var clearSearch = document.getElementById("clear-search");
 var mediaFolder = "https://filedn.com/lHHBHWtAlOt5syqI4onfcmB/NJF/media/";
 
 clearSearch.onclick = function() {
-    input.value = "";
-    search();
+	input.value = "";
+	search();
 };
 
 input.addEventListener('input', search);
 
-$.getJSON("https://api.myjson.com/bins/xjra8", function(json) {
+$.getJSON("episodes.json", function(json) {
+// $.getJSON("https://api.myjson.com/bins/xjra8", function(json) {
     episodes = json.sort(function(a, b) {
         return parseFloat(a.episode) - parseFloat(b.episode);
     });
@@ -29,49 +30,49 @@ $.getJSON("https://api.myjson.com/bins/xjra8", function(json) {
 });
 
 function displayEpisode(episode) {
-    var container = document.createElement("div");
-    container.innerText = "Episode " + episode.episode + ": " + episode.title;
-    container.className = "episode clickable";
-    container.onclick = function() {
-        currentEpisode = episodes.indexOf(episode);
-      loadEpisode();
-    };
+	var container = document.createElement("div");
+	container.innerText = "Episode " + episode.episode + ": " + episode.title;
+	container.className = "episode clickable";
+	container.onclick = function() {
+		currentEpisode = episodes.indexOf(episode);
+		loadEpisode();
+	};
 
-    playlist.appendChild(container);
+	playlist.appendChild(container);
 }
 
 function search() {
-    var search = input.value;
-    playlist.innerHTML = "";
-    if(search.length > 0) {
-        searchIcon.style.display = "none";
-        clearSearch.style.display = "inline-block";
-        episodes.forEach(function(episode) {
-            if(episode.title.toLowerCase().includes(search.toLowerCase()) || episode.episode.toString().startsWith(search)) {
-                displayEpisode(episode);
-            }
-        });
-    } else {
-        searchIcon.style.display = "inline-block";
-        clearSearch.style.display = "none";
-        episodes.forEach(function(episode) {
-            displayEpisode(episode);
-        });
-    }
+	var search = input.value;
+	playlist.innerHTML = "";
+	if(search.length > 0) {
+		searchIcon.style.display = "none";
+		clearSearch.style.display = "inline-block";
+		episodes.forEach(function(episode) {
+			if(episode.title.toLowerCase().includes(search.toLowerCase()) || episode.episode.toString().startsWith(search)) {
+				displayEpisode(episode);
+			}
+		});
+	} else {
+		searchIcon.style.display = "inline-block";
+		clearSearch.style.display = "none";
+		episodes.forEach(function(episode) {
+			displayEpisode(episode);
+		});
+	}
 }
 
 function loadEpisode() {
-    var episode = episodes[currentEpisode];
-    audio.setAttribute("src", mediaFolder + episode.src);
-    audio.currentTime = episode.start;
-    audio.play();
+	var episode = episodes[currentEpisode];
+	audio.setAttribute("src", mediaFolder + episode.src);
+	audio.currentTime = episode.start;
+	audio.play();
 
-    document.getElementById("title").innerText = episode.title;
-    document.getElementById("episode").innerText = "Episode " + episode.episode;
-    document.getElementById("youtube-button").onclick = function() {
-        window.open("http://youtu.be/" + episode.id + "?t=" + episode.start);
-    };
-    document.getElementById("download-button").onclick = function() {
-        window.open(mediaFolder + episode.src);
-    };
+	document.getElementById("title").innerText = episode.title;
+	document.getElementById("episode").innerText = "Episode " + episode.episode;
+	document.getElementById("youtube-button").onclick = function() {
+		window.open("http://youtu.be/" + episode.id + "?t=" + episode.start);
+	};
+	document.getElementById("download-button").onclick = function() {
+		window.open(mediaFolder + episode.src);
+	};
 }
